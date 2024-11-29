8703ad46346fa7e5c405b250d51af52b644527e4
Viktoriya Bokova <Viktoriya.Bokova@acronis.com>
2024-11-29 18:46:19 +0300

    new

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,41 @@
 package main
 
-// реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+import "fmt"
+
+// calculator - функция, которая принимает три канала:
+// firstChan, secondChan, stopChan, и возвращает канал resChan типа <-chan int.
+// Она запускает горутину, которая обрабатывает значения из каналов.
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	resChan := make(chan int) // Создаем канал для результата
+
+	go func() { // Запускаем анонимную горутину
+		defer close(resChan) // Закрываем канал при завершении
+
+		// select позволяет ожидать значения из любого из каналов
+		select {
+		// Получение значения из firstChan
+		case val := <-firstChan: // Получаем значение из канала
+			resChan <- val * val // Квадрат значения и отправка в resChan
+		// Получение значения из secondChan
+		case val := <-secondChan: // Получаем значение из канала
+			resChan <- val * 3 // Умножение значения на 3 и отправка в resChan
+		// Получение сигнала из stopChan
+		case <-stopChan: // Получаем сигнал из канала
+			return // Завершаем горутину
+		}
+	}()
+
+	return resChan // Возвращаем канал результата
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	ch1 := make(chan int)      // Создаем канал ch1
+	ch2 := make(chan int)      // Создаем канал ch2
+	ch3 := make(chan struct{}) // Создаем канал ch3 (для сигнала остановки)
+
+	res := calculator(ch1, ch2, ch3) // Вызываем функцию calculator и получаем канал res
+
+	ch2 <- 16 // Отправляем значение 16 в ch2
+
+	fmt.Println(<-res) // Получаем результат из канала res и выводим его
 }
--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,38 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import "fmt"
+
+func removeDuplicates(inputStream <-chan string, outputStream chan<- string) {
+	defer close(outputStream) // Закрываем выходной канал при завершении
+
+	var prevValue string // Храним предыдущее значение
+	for value := range inputStream {
+		if value != prevValue { // Проверка на дубликат
+			outputStream <- value // Отправка в выходной канал, если значение не дубликат
+			prevValue = value     // Обновляем предыдущее значение
+		}
+	}
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	in := make(chan string)
+	out := make(chan string)
+
+	go func() {
+		defer close(in) // Закрываем входной канал при завершении
+		in <- "one"
+		in <- "two"
+		in <- "two"
+		in <- "three"
+		in <- "three"
+		in <- "three"
+		in <- "four"
+		in <- "five"
+	}()
+
+	go removeDuplicates(in, out)
+
+	for value := range out {
+		fmt.Println(value)
+	}
 }
